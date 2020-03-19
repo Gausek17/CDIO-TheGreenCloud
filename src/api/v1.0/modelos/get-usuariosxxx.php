8b0810e6bf3791aec7b0166828c49a4afa585c09
Leire <lvilmar1@epsg.upv.es>
2020-03-19 23:50:49 +0100

    Revert "gitflow-feature-stash: resubida"
    This reverts commit 8a802c6c2ae09c680f548a40e58fec41591be307.

--- src/api/v1.0/modelos/get-usuariosxxx.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sql = 'SELECT * FROM `usuarios`';
-$res = mysqli_query($conexion, $sql);
-
-while ($fila = mysqli_fetch_assoc($res)) {
-    array_push($salida, $fila);
-}
-$http_code = 200;
