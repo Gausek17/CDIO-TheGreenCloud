b2ed96f6ea483a1caf7f76023d79b669fcf3d7e8
Juan Carlos <jcherram@epsg.upv.es>
2020-04-03 11:06:36 +0200

    Correcciones para vista en servidor

--- src/app/js/menuAdmin.js
+++ src/app/js/menuAdmin.js
@@ -10,7 +10,7 @@ let VistaMenu = {
             <a href="ParcelasAdmin.php" class="itemNav"><img src="../imagenes/menu/IconoCampo-White.png" alt="Imagen Campo" class="iconParcelas"><span class="textNav">Parcelas</span></a>
         </li>
         <li class="navUsuarios">
-            <a href="Index.html" class="itemNav"><img src="../imagenes/menu/groupIcon-White.png" alt="Gestión Usuarios" class="iconGestionUsuarios"><span class="textNav">Gestión Usuarios</span></a>
+            <a href="index.html" class="itemNav"><img src="../imagenes/menu/groupIcon-White.png" alt="Gestión Usuarios" class="iconGestionUsuarios"><span class="textNav">Gestión Usuarios</span></a>
         </li>
         <li class="navMediciones">
                   <a href="graficasAdmin.html" class="itemNav"><img src="../imagenes/menu/sensor.png" alt="Imagen Sensor" class="iconMediciones"><span class="textNav">Mediciones</span></a>
@@ -24,7 +24,7 @@ let VistaMenu = {
     crearSideNav: function () {
         this.sideNav.innerHTML = `<div class="headerMenu"><span class="menuTextAdmin">Admin Panel</span><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a></div>
             <a href="ParcelasAdmin.php" class="itemMenu"><img src="../imagenes/menu/IconoCampo-White.png" alt="imagenCampo" class="iconParcelas"><span class="menuText">Parcelas</span></a>
-            <a href="Index.html" class="itemMenu"><img src="../imagenes/menu/groupIcon-White.png" alt="Imagen Usuarios" class="iconGestionUsuarios"><span class="menuText">Gestión Usuarios</span></a>
+            <a href="index.html" class="itemMenu"><img src="../imagenes/menu/groupIcon-White.png" alt="Imagen Usuarios" class="iconGestionUsuarios"><span class="menuText">Gestión Usuarios</span></a>
             <a href="graficasAdmin.html" class="itemMenu"><img src="../imagenes/menu/sensor.png" alt="Imagen Sensor" class="iconMediciones"><span class="menuText">Mediciones</span></a>`;
     }
 };
