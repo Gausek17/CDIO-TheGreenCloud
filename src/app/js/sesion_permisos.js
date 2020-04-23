527549b3230aca08e1ad376f6ab8ecce5af091da
Juan Carlos <jcherram@epsg.upv.es>
2020-04-23 17:26:05 +0200

    Reedirigir Cerrar Sesion  a Login

--- src/app/js/sesion_permisos.js
+++ src/app/js/sesion_permisos.js
@@ -8,6 +8,6 @@ fetch('../api/v1.0/sesion', {method:'get'}).then(function (respuesta) {
 function deleteSesion() {
     fetch('../api/v1.0/sesion',{method:'delete'}).then(
         function(respuesta) {
-            if(respuesta.status === 200) location.href = '..';
+            if(respuesta.status === 200) location.href = '../login.html';
         })
 }
