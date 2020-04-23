0815e7be29b0dbae5b99a41f67c188803aaa07d8
Juan Carlos <jcherram@epsg.upv.es>
2020-04-23 18:29:20 +0200

    Correcciones errores diseño

--- src/api/includes/conexion.php
+++ src/api/includes/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-
+/*Local*/
 $bbdd_server = 'localhost';
 $bbdd_user = 'root';
 $bbdd_password = '';
@@ -9,3 +9,16 @@ $bbdd = 'bd_tgc';
 $conexion = mysqli_connect($bbdd_server,$bbdd_user,$bbdd_password,$bbdd);
 
 mysqli_query($conexion, 'SET NAMES utf8');
+
+
+
+/*Servidor
+$bbdd_server = 'localhost';
+$bbdd_user = 'jcherram_jchherram_user';
+$bbdd_password = '1vys8T@2';
+$bbdd = 'jcherram_bd_tgc';
+
+$conexion = mysqli_connect($bbdd_server,$bbdd_user,$bbdd_password,$bbdd);
+
+mysqli_query($conexion, 'SET NAMES utf8');
+*/
