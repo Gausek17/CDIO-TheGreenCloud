ab3150a4b75f187c9cff92cd4f308878c4df4600
ALBERTO <albertitovalls97@gmail.com>
2020-05-08 01:00:18 +0200

    cambios gestion parcelas

---
+++ src/app/js/modalEdit.js
@@ -0,0 +1,32 @@
+function modalEdit(id){
+    let stringPopUp = `
+    <div class="w3-modal-content w3-card-4">
+        <header class="w3-container w3-teal"> 
+        <span onclick="closeModal()"
+              class="w3-button w3-large w3-display-topright">&times;</span>
+            <h2>Gestión de Parcelas</h2>
+            
+        </header>
+        
+        <div class="w3-container">
+            <p>El usuario se borrará permanentemente</p>
+            <button class="botonConfirm" alt="Confirm" onclick="optionConfirm(${id})">Confirmar</button>
+            <button class="botonCancel" alt="Cancel" onclick="closeModal()">Cancelar</button>
+            
+        </div>
+    </div>
+    `;
+    let element = document.getElementById("divPopup");
+    element.innerHTML = stringPopUp;
+    element.style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('divPopup').style.display='none'
+}
+
+
+function optionConfirm(id) {
+    deleteUser(id);
+    closeModal();
+}
--- src/app/js/usuarios_div.js
+++ src/app/js/usuarios_div.js
@@ -34,6 +34,7 @@ let VistaDivUsuarios = {
                     
                     <button alt="Aceptar" class="iconAceptarCancel" id="iconAceptar${datos[i].id_usuario}" onclick="aceptar(${datos[i].id_usuario})">Aceptar cambios</button>
                     <button alt="Cancelar" class="iconAceptarCancel"  id="iconCancel${datos[i].id_usuario}" onclick="cancelar(${datos[i].id_usuario})">Cancelar cambios</button>
+                    <img src="../imagenes/menu/IconoCampo-White.png" alt="Editar parcelas" class="iconAsign" id="iconSelectParcelas${datos[i].id_usuario}" onclick="modalEdit(${datos[i].id_usuario})">
                     <img src="../imagenes/admin/iconDelete-white.png" alt="Elimina usuario" class="iconEditUser1"  id="iconDelete${datos[i].id_usuario}" onclick="modalConfirmar(${datos[i].id_usuario})">
                     
                 </div>`;
@@ -199,6 +200,7 @@ function filtrar() {
                     <button alt = "Editar usuario" class="iconEditUser1" id="iconEditar${datos[i].id_usuario}" onclick="editUser(${datos[i].id_usuario})">Editar Usuario </button>
                     
                     <button alt="Aceptar" class="iconAceptarCancel" id="iconAceptar${datos[i].id_usuario}" onclick="aceptar(${datos[i].id_usuario})">Aceptar cambios</button>
+                    <button alt="Editar Parcelas" class="iconEditParcelas"  id="iconParcelas${datos[i].id_usuario}" onclick="modalEdit(${datos[i].id_usuario})"> </button>
                     <button alt="Elimina usuario" class="iconEditUser1"  id="iconDelete${datos[i].id_usuario}" onclick="modalConfirmar(${datos[i].id_usuario})">Eliminar Usuario </button>
                     <button alt="Cancelar" class="iconAceptarCancel"  id="iconCancel${datos[i].id_usuario}" onclick="cancelar(${datos[i].id_usuario})">Cancelar cambios</button>
                 </div>`;
