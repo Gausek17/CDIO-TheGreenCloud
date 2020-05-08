3679bc84f8d84d622d7ba301732a54d4a06a2427
Leire <lvilmar1@epsg.upv.es>
2020-05-08 18:22:25 +0200

    añadir noticias al menu

--- src/app/js/menuUser.js
+++ src/app/js/menuUser.js
@@ -12,6 +12,9 @@ let VistaMenu = {
                 <li class="navMediciones">
                   <a href="graficasUser.html" class="itemNav"><img src="../imagenes/menu/sensor.png" alt="Imagen Sensor" class="iconMediciones"><span class="textNav">Mediciones</span></a>
                  </li>
+                <li class="navNoticias">
+                  <a href="graficasAdmin.html" class="itemNav"><img src="../imagenes/menu/sensor.png" alt="Imagen Sensor" class="iconNoticias"><span class="textNav">Noticias</span></a>
+                </li>
             </ul>`;
     },
     sideNav: {},
@@ -21,7 +24,8 @@ let VistaMenu = {
     crearSideNav: function () {
         this.sideNav.innerHTML = `<div class="headerMenu"><span class="menuTextAdmin">User Panel</span><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a></div>
             <a id="b1" href="parcelasUser.html" class="itemMenu"><img src="../imagenes/menu/IconoCampo-White.png" alt="Imagen Campo" class="iconParcelas"><span class="menuText">Parcelas</span></a>
-            <a id="b2" href="graficasUser.html" class="itemMenu"><img src="../imagenes/menu/sensor.png" alt="Imagen Sensor" class="iconMediciones"><span class="menuText">Mediciones</span></a>`;
+            <a id="b2" href="graficasUser.html" class="itemMenu"><img src="../imagenes/menu/sensor.png" alt="Imagen Sensor" class="iconMediciones"><span class="menuText">Mediciones</span></a>
+            <a id="a4" href="#" class="itemMenu"><img src="../imagenes/menu/sensor.png" alt="Imagen Noticias" class="iconNoticias"><span class="menuText">Noticias</span></a>`;
     }
 };
 
