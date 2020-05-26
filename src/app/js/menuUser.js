afc7addd7bf7155c43aff53e1ca6d3cc6275ef5a
ALBERTO <albertitovalls97@gmail.com>
2020-05-26 17:11:05 +0200

    Pagina Tiempo y menu

--- src/app/js/menuUser.js
+++ src/app/js/menuUser.js
@@ -12,8 +12,8 @@ let VistaMenu = {
                 <li class="navMediciones">
                   <a href="graficasUser.html" class="itemNav"><i class="fas fa-chart-bar"></i>Mediciones</span></a>
                  </li>
-                <li class="navNoticias">
-                  <a href="noticias.html" class="itemNav"><i class="fas fa-cloud-sun"></i>Tiempo</span></a>
+                <li class="navTiempo">
+                  <a href="TiempoUser.html" class="itemNav"><i class="fas fa-cloud-sun"></i>Tiempo</span></a>
                 </li>
             </ul>`;
     },
@@ -25,7 +25,7 @@ let VistaMenu = {
         this.sideNav.innerHTML = `<div class="headerMenu"><span class="menuTextAdmin">User Panel</span><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a></div>
             <a id="b1" href="parcelasUser.html" class="itemMenu"><i class="fas fa-leaf"></i>Ver Parcelas</span></a>
             <a id="b2" href="graficasUser.html" class="itemMenu"><i class="fas fa-chart-bar"></i>Mediciones</span></a>
-            <a id="b3" href="#" class="itemMenu"><i class="fas fa-cloud-sun"></i>Tiempo</span></a>
+            <a id="b3" href="TiempoUser.html" class="itemMenu"><i class="fas fa-cloud-sun"></i>Tiempo</span></a>
             <i class="fas fa-sign-out-alt" onclick="deleteSesion()"></i>`;
     }
 
